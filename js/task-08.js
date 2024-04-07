a8767194d70063fa467d1b6504f90f0f8b62214c
SaraSmarandoiu <smarandoiusara@gmail.com>
2024-04-07 13:16:45 +0300

    commit

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const formElements = loginForm.elements;
 
+  const formData = {};
+  for (let i = 0; i < formElements.length; i++) {
+    const element = formElements[i];
+
+    if (element.tagName === "INPUT" && element.value.trim() !== "") {
+      formData[element.name] = element.value;
+    } else if (element.tagName === "INPUT") {
+      alert("Please fill in all fields.");
+      return;
+    }
+  }
+  console.log(formData);
+  loginForm.reset();
+});
