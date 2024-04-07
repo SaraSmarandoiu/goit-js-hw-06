7f90da903b09bae6e6bbe1ec0eb7ca421be15b5b
SaraSmarandoiu <smarandoiusara@gmail.com>
2024-04-07 13:23:02 +0300

    commit

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const input = document.querySelector("input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxesContainer = document.getElementById("boxes");
+createButton.addEventListener("click", createBoxes);
+destroyButton.addEventListener("click", destroyBoxes);
+function createBoxes() {
+  destroyBoxes();
+
+  const amount = parseInt(input.value);
+  if (amount > 0 && amount <= 100) {
+    let size = 30;
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement("div");
+      box.style.width = `${size}px`;
+      box.style.height = `${size}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      boxesContainer.appendChild(box);
+      size += 10;
+    }
+  } else {
+    alert("Please enter a number between 1 and 100.");
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
